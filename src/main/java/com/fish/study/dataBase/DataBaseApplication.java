ef96815fabf4416fc3be69f7daa796163bb04f97
fish <yxzjerryfish@hotmail.com>
2019-10-06 21:03:49 +0800

    spring boot学习

---
+++ src/main/java/com/fish/study/dataBase/DataBaseApplication.java
@@ -0,0 +1,41 @@
+package com.fish.study.dataBase;
+
+import com.fish.study.dataBase.mapper.CoffeeMapper;
+import com.fish.study.dataBase.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan("com.fish.study.dataBase.mapper")
+public class DataBaseApplication implements ApplicationRunner {
+
+	@Autowired
+	private CoffeeMapper coffeeMapper;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Coffee c = Coffee.builder().name("espresso")
+				.price(20).build();
+
+		int count = coffeeMapper.save(c);
+
+		c = Coffee.builder().name("latte")
+				.price(25).build();
+
+		count = coffeeMapper.save(c);
+		log.info("count Coffee: {},{}", count,c);
+
+		c = coffeeMapper.findById(1L);
+		log.info("Find Coffee: {}", c);
+	}
+}
